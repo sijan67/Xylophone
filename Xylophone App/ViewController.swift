0098108d33d1c4817297a41a4ee023b1e8578dfc
sijan67 <sijuichi1@gmail.com>
2022-01-10 12:58:07 +0900

    Added sound playing functionality

--- Xylophone App/ViewController.swift
+++ Xylophone App/ViewController.swift
@@ -6,9 +6,13 @@
 //
 
 import UIKit
+import AVFoundation
+var player: AVAudioPlayer?
+// capable of interacting with audio hardware
 
 class ViewController: UIViewController {
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -16,7 +20,42 @@ class ViewController: UIViewController {
 
 
     @IBAction func keyPressed(_ sender: UIButton) {
-        print("I got pressed")
+        
+        let soundName = sender.titleLabel?.text
+//        print(soundName!)
+        
+        //Reduces the sender's (the button that got pressed) opacity to half.
+          sender.alpha = 0.5
+
+          //Code should execute after 0.2 second delay.
+          DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+              //Bring's sender's opacity back up to fully opaque.
+              sender.alpha = 1.0
+          }
+        
+        playSound(name: soundName!)
+        
     }
+    
+    func playSound(name: String) {
+        guard let url = Bundle.main.url(forResource: name, withExtension: "wav") else { return }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.wav.rawValue)
+
+
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+
 }
 
